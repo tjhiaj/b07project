221523677ceb87611bf67df39e98b9e4fef561a2
CherylZ4 <112735556+CherylZ4@users.noreply.github.com>
2023-11-13 13:14:15 -0500

    Fix project build

--- app/src/main/java/com/example/b07project/MainActivity.kt
+++ app/src/main/java/com/example/b07project/MainActivity.kt
@@ -10,15 +10,18 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.b07project.ui.theme.B07ProjectTheme
+import com.example.b07project.ui.theme.B07projectTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            B07ProjectTheme {
+            B07projectTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
                     Greeting("Android")
                 }
             }
@@ -29,15 +32,15 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-            text = "Hello $name!",
-            modifier = modifier
+        text = "Hello $name!",
+        modifier = modifier
     )
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    B07ProjectTheme {
+    B07projectTheme {
         Greeting("Android")
     }
 }
