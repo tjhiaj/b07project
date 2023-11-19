903019919cbe4c5d4ebca182e6058a345ae3627e
tjhiaj <j.tjhia@gmail.com>
2023-11-19 13:45:42 -0500

    Button navigation and announcements display.
    Added activities to manage button navigation between pages.
    Created announcement adapter for recycler view and announcement java class.

---
+++ app/src/main/java/com/example/b07project/AdminHomeActivity.java
@@ -0,0 +1,19 @@
+package com.example.b07project;
+
+import android.content.Intent;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class AdminHomeActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_home);
+    }
+    public void onAdminAnnouncementsButtonClick(View view) {
+        Intent intent = new Intent(this, AdminAnnouncementsActivity.class);
+        startActivity(intent);
+    }
+}
