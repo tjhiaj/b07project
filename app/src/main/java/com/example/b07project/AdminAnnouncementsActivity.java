34cfa203f7b3d0bc91933fda1187b3d0d0560aad
CherylZ4 <112735556+CherylZ4@users.noreply.github.com>
2023-11-20 15:03:02 -0500

    Stuff

--- app/src/main/java/com/example/b07project/AdminAnnouncementsActivity.java
+++ app/src/main/java/com/example/b07project/AdminAnnouncementsActivity.java
@@ -1,13 +1,111 @@
 package com.example.b07project;
 
-import android.os.Bundle;
+import static android.content.ContentValues.TAG;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.LinkedList;
+
 public class AdminAnnouncementsActivity extends AppCompatActivity {
+    FirebaseDatabase database;
+    TextView textView ;
+    Button post;
+
+
+    LinkedList<String> announcementsList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_announcements_admin);
+        FirebaseDatabase.getInstance().setPersistenceEnabled(false);
+        database = FirebaseDatabase.getInstance("https://b07project-7eb3d-default-rtdb.firebaseio.com/");
+
+        post = findViewById(R.id.postButton);
+        announcementsList = new LinkedList<>();
+
+        if(database == null) return;
+
+        post.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //DO I need to add announcements thing in firebase
+                DatabaseReference ref = database.getReference("announcements");
+                EditText complaintContent = findViewById(R.id.messageEditText);
+                EditText subjectContent = findViewById(R.id.subjectEditText);
+                String announcements = complaintContent.getText().toString();
+                String subjects = subjectContent.getText().toString();
+
+                if(ref == null) return;
+                if (!subjects.isEmpty()) {
+                    DatabaseReference subjectRef = ref.child(subjects);
+                    announcementsList.addFirst(announcements);
+                    subjectRef.setValue(announcements).addOnCompleteListener(new OnCompleteListener<Void>() {
+
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+
+                            if (task.isSuccessful()) {
+                                // Update UI or show a toast indicating successful update
+                                complaintContent.setText("");
+                                subjectContent.setText("");
+                                Toast.makeText(AdminAnnouncementsActivity.this, "Announcement submitted", Toast.LENGTH_SHORT).show();
+                                Intent intent = new Intent(getApplicationContext(), AdminHomeActivity.class);
+                                startActivity(intent);
+                                finish();
+                            } else {
+                                Exception exception = task.getException();
+                                Log.e(TAG, "Failed to submit announcement: " + exception.getMessage(), exception);
+                            }
+                        }
+                    });
+                    ref.child(subjects).addListenerForSingleValueEvent(new ValueEventListener() {
+
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                            // Log the actual data
+                            if (dataSnapshot.exists()) {
+                                //Log.d(TAG, "Data: " + dataSnapshot.getValue());
+                            } else {
+                                //Log.d(TAG, "Data does not exist");
+                            }
+                        }
+
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError databaseError) {
+                            Log.e(TAG, "Error reading data: " + databaseError.getMessage(), databaseError.toException());
+                        }
+                    });
+
+
+
+                } else {
+                    Toast.makeText(AdminAnnouncementsActivity.this, "Subject cannot be empty", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
+        }
+
+
     }
-}
+
