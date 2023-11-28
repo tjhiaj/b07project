220d3eb537540e0942414f723d88200dca4b98e7
Andy <mazhiyuannn@gmail.com>
2023-11-28 14:54:04 -0500

    the major and specialist views are working now, but still in progress

--- app/src/main/java/com/example/b07project/CSRequirementsActivity.java
+++ app/src/main/java/com/example/b07project/CSRequirementsActivity.java
@@ -10,33 +10,61 @@ import android.widget.Toast;
 import com.google.android.material.snackbar.Snackbar;
 
 public class CSRequirementsActivity extends AppCompatActivity {
+    CheckBox checkbox1;
+    CheckBox checkbox2;
+    CheckBox checkbox3;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_computer_science_requirements);
+
+        checkbox1 = findViewById(R.id.CS_minimum_gpa_box);
+        checkbox2 = findViewById(R.id.CS_minimum_grade_box);
+        checkbox3 = findViewById(R.id.CS_minimum_2_grades_box);
     }
-    CheckBox checkbox1 = findViewById(R.id.CS_minimum_gpa_box);
-    CheckBox checkbox2 = findViewById(R.id.CS_minimum_grade_box);
-    CheckBox checkbox3 = findViewById(R.id.CS_minimum_2_grades_box);
 
-    protected void onCSReqSubmitClick(View view){
+    public void onCSReqSubmitClick(View view){
         if (checkbox1.isChecked() && checkbox2.isChecked() && checkbox3.isChecked()) {
+            String message1 = "You qualify for UTSC's Computer Science POSt requirements and can" +
+                    "enroll in either a specialist or major CS program. Good job! Keep it up.";
+            Snackbar snackbar = Snackbar.make(view, message1, Snackbar.LENGTH_INDEFINITE);
 
+            // Add an action (e.g., close button)
+            snackbar.setAction("Close", new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    snackbar.dismiss(); // Dismiss the Snackbar when the action is clicked
+                }
+            });
 
-//            String message = "You qualify for UTSC's Computer Science POSt requirements and can" +
-//                    "enroll in either a specialist or major CS program. Good job! Keep it up.";
+            snackbar.show();
 //            int duration = Toast.LENGTH_SHORT; // Duration for how long the toast message will appear
 //
 //            Toast toast = Toast.makeText(getApplicationContext(), message, duration);
 //            toast.show();
         } else {
-            String message = "Sorry, you do not qualify for UTSC's Computer Science POSt " +
-                    "requirements and you may not enroll in either a specialist or major CS program." +
-                    "Good luck in your future endeavors.";
-            int duration = Toast.LENGTH_SHORT; // Duration for how long the toast message will appear
+            String message2 = "Sorry, you do not qualify for UTSC's Computer Science POSt " +
+                    "requirements and you may not enroll in either a specialist or major CS " +
+                    "program. Good luck in your future endeavors.";
+            Snackbar snackbar = Snackbar.make(view, message2, Snackbar.LENGTH_INDEFINITE);
 
-            Toast toast = Toast.makeText(getApplicationContext(), message, duration);
-            toast.show();
+            // Add an action (e.g., close button)
+            snackbar.setAction("Close", new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    snackbar.dismiss(); // Dismiss the Snackbar when the action is clicked
+                }
+            });
+
+            snackbar.show();
+//            String message2 = "Sorry, you do not qualify for UTSC's Computer Science POSt " +
+//                    "requirements and you may not enroll in either a specialist or major CS program." +
+//                    "Good luck in your future endeavors.";
+//            int duration = Toast.LENGTH_SHORT; // Duration for how long the toast message will appear
+//
+//            Toast toast = Toast.makeText(getApplicationContext(), message2, duration);
+//            toast.show();
         }
     }
 }
