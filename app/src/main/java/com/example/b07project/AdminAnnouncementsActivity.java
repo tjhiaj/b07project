fe7c15f4eee30efb97f422b9ccb8c50c561a0e4d
Andy <mazhiyuannn@gmail.com>
2023-12-01 13:19:39 -0500

    saving

--- app/src/main/java/com/example/b07project/AdminAnnouncementsActivity.java
+++ app/src/main/java/com/example/b07project/AdminAnnouncementsActivity.java
@@ -62,8 +62,7 @@ public class AdminAnnouncementsActivity extends AppCompatActivity {
                         public void onComplete(@NonNull Task<Void> task) {
 
                             if (task.isSuccessful()) {
-                                // Notification creation logic
-                                NotificationHelper.showNotification(AdminAnnouncementsActivity.this, "New Announcement", subjects + " - " + announcements);
+                                NotificationHelper.showNotification(AdminAnnouncementsActivity.this, "New Announcement!", subjects + " - " + announcements);
                                 // Update UI or show a toast indicating successful update
                                 complaintContent.setText("");
                                 subjectContent.setText("");
