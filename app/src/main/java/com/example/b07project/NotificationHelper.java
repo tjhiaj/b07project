e4d79961dfe24580c24be7780a0ea8b558cdff81
Andy <mazhiyuannn@gmail.com>
2023-12-01 09:50:54 -0500

    can now click announcement noti to go to announcements

--- app/src/main/java/com/example/b07project/NotificationHelper.java
+++ app/src/main/java/com/example/b07project/NotificationHelper.java
@@ -2,7 +2,9 @@ package com.example.b07project;
 
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Build;
 
 import androidx.core.app.NotificationCompat;
@@ -14,7 +16,8 @@ public class NotificationHelper {
     private static final String CHANNEL_DESCRIPTION = "I DONT KNOW WHAT IM DOING";
 
     public static void showNotification(Context context, String title, String message) {
-        NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        NotificationManager notificationManager = (NotificationManager)
+                context.getSystemService(Context.NOTIFICATION_SERVICE);
 
         if (notificationManager == null) {
             return;
@@ -31,10 +34,17 @@ public class NotificationHelper {
             notificationManager.createNotificationChannel(channel);
         }
 
-        android.app.Notification.Builder builder = new android.app.Notification.Builder(context, CHANNEL_ID)
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, CHANNEL_ID)
                 .setSmallIcon(R.drawable.pixel_kirby) // Set your notification icon here
                 .setContentTitle(title)
-                .setContentText(message);
+                .setContentText(message)
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+        Intent intent = new Intent(context, StudentAnnouncementsActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent,
+                PendingIntent.FLAG_IMMUTABLE);
+        builder.setContentIntent(pendingIntent);
 
         // Display the notification
         notificationManager.notify(0, builder.build());
